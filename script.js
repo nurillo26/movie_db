b24bfc31fa0798bf12572e957d661ee36f1885df
Nurlan <nurikrahimbaev26@gmail.com>
2021-10-26 13:39:08 +0600

    lesson 15 practice 2

--- script.js
+++ script.js
@@ -10,13 +10,29 @@ const personalMovieDB = {
     privat: false,
 };
 
-const lastMovie1 = prompt('Один из последних просмотренных фильмов?');
-const movieRating1 = prompt('На сколько его оцените?');
-const lastMovie2 = prompt('Один из последних просмотренных фильмов?');
-const movieRating2 = prompt('На сколько его оцените?');
 
-personalMovieDB.movies[lastMovie1] = movieRating1;
-personalMovieDB.movies[lastMovie2] = movieRating2;
+for (let i = 0; i < 2; i++) {
+    const lastMovie1 = prompt('Один из последних просмотренных фильмов?');
+    const movieRating1 = prompt('На сколько его оцените?');
+
+    if (lastMovie1 != null && movieRating1 != null && lastMovie1 != '' && movieRating1 != '' && lastMovie1.length < 50) {
+        personalMovieDB.movies[lastMovie1] = movieRating1;
+        console.log('done')
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
 
 
 console.log(personalMovieDB);
